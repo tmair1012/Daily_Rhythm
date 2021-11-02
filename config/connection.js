f9fb97b6b636226477fef32105e2c2d723ce9ca5
TylerMair <tyleramair@gmail.com>
2021-11-01 20:46:16 -0700

    connection

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Moxie2020!',
+    database: 'employeesTracker'
+
+});
+
+module.exports = db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS dailyRhythm;
 
 CREATE DATABASE dailyRhythm;
+
+
