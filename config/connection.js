82d8546c5fcff93a475f40c9b6fcedbbd5defc70
Alejandro Machado Rosas <alejandromrosas@Alejandros-MacBook-Pro.local>
2021-11-04 16:08:32 -0700

    code

--- config/connection.js
+++ config/connection.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.schemas.sequelize);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3001
   });
 }
 
-module.exports = sequelize;
+module.exports = sequelize
