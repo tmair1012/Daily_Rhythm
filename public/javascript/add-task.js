30627e9f0ee368d2b4d496c698491c5182273fe2
TylerMair <tyleramair@gmail.com>
2021-11-04 13:28:04 -0700

    add task handlebars

--- public/javascript/add-task.js
+++ public/javascript/add-task.js
@@ -2,13 +2,13 @@ async function newtask(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="taskTitle"').value;
-
+    const description = document.querySelector
     const response = await fetch(`/api/task-routes`, {
         method: 'POST',
         body: JSON.stringify(
             {
             title,
-            
+
         })
     } )
 }
