86218fe82f1e7963c7769d639787ad2dbb9ab065
TylerMair <tyleramair@gmail.com>
2021-11-04 14:17:44 -0700

    stored sql data in env file and added to git ignore

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,11 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-if (process.env.JAWSDB_URL) {
-  const sequelize = process.env.JAWSDB_URL
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_TASK, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3301
-  });
-}
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 
 module.exports = sequelize;
--- db/schema 2.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS dailyRhythm;
-
-CREATE DATABASE dailyRhythm;
-
-
