d5e326215ad97f51ba9e3ef7025a0542264298c4
TylerMair <tyleramair@gmail.com>
2021-11-08 20:27:35 -0700

    working through adding task

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const taskRoutes = require('./task-routes');
 const userRoutes = require('./user_routes');
 
-router.use('/task', taskRoutes);
+router.use('/tasks', taskRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
