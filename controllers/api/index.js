0e373ce1f19046ae1ce23a0fbd7d7e67eb4fa61c
TylerMair <tyleramair@gmail.com>
2021-11-08 19:46:07 -0700

    css linked, login and signup work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
-
-
 const taskRoutes = require('./task-routes');
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user_routes');
 
 router.use('/task', taskRoutes);
 router.use('/users', userRoutes);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 const express = require('express');
 const exphbs = require('express-handlebars');
-
+const session = require('express-session');
 const sequelize = require('./config/connection');
 const path = require('path');
 
@@ -9,28 +9,38 @@ const hbs = exphbs.create({});
 
 
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(require('./controllers'));
 
 
 // create sequelize session using express-session and sequelize store
-const expressSession = require('express-session');
-const SessionStore = require('express-session-sequelize')(expressSession.Store);
-const Sequelize = require('sequelize');
-const myDatabase = sequelize;
-const sequelizeSessionStore = new SessionStore({
-  db: myDatabase,
-});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+
+const sess = {
+  secret: 'wow secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 
 // middleware connects handlebars, requires routes
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 
 // turn on connection to db and server - connecting to sequelize server
